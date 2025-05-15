250d757ef36de564d1dc89766742d012e5e8a9e6
Zulal Argin <zulal.argin@hotmail.com>
2025-05-15 22:01:57 +0400

    init

---
+++ src/main/java/com/zulal/sitecrew/SiteCrewManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zulal.sitecrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCrewManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCrewManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zulal/sitecrew/SiteCrewManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zulal.sitecrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteCrewManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
